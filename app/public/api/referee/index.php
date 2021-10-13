3bb06d5de1ef35004f63b15c78ebe861a5c5156e
adisahoo <adisahoo@iu.edu>
2021-10-13 19:19:52 -0400

    modified files

---
+++ app/public/api/referee/index.php
@@ -0,0 +1,36 @@
+<?php
+// require 'common.php';
+require 'class/DbConnection.php';
+
+// echo ":::";
+// // print_R($_ENV);
+// echo getenv('MYSQL_DATABASE');
+
+
+// Step 1: Get a datase connection from our helper class
+$db = DbConnection::getConnection();
+
+// echo $db;
+// exit;
+
+// Step 2: Create & run the query
+$sql = 'SELECT * FROM referees';
+$vars = [];
+
+//  if (isset($_GET['referee'])) {
+//    // This is an example of a parameterized query
+//    $sql = 'SELECT * FROM offer WHERE studentID = ?';
+//    $vars = [ $_GET['student'] ];
+//  }
+
+$stmt = $db->prepare($sql);
+$stmt->execute($vars);
+
+$referees = $stmt->fetchAll();
+
+// Step 3: Convert to JSON
+$json = json_encode($referees, JSON_PRETTY_PRINT);
+
+// Step 4: Output
+header('Content-Type: application/json');
+echo $json;
---
+++ app/public/js/index.js
@@ -0,0 +1,60 @@
+const SomeApp = {
+    data() {
+      return {
+        "referees": [],
+        "assignments": [],
+        "games": []
+      }
+    },
+    computed: {},
+    methods: {
+        prettyData(d) {
+            return dayjs(d)
+            .format('D MMM YYYY')
+        },
+        prettyDollar(n) {
+            const d = new Intl.NumberFormat("en-US").format(n);
+            return "$ " + d;
+        },
+        fetchRefereeData() {
+            fetch('/api/referee/')
+            .then( response => response.json() )
+            .then( (responseJson) => {
+                console.log(responseJson);
+                this.referees = responseJson;
+            })
+            .catch( (err) => {
+                console.error(err);
+            })
+        },
+        fetchGameData() {
+            fetch('/api/game/' )
+            .then( response => response.json() )
+            .then( (responseJson) => {
+                console.log(responseJson);
+                this.games = responseJson;
+            })
+            .catch( (err) => {
+                console.error(err);
+            })
+        },
+        fetchAssignmentData() {
+            fetch('/api/assignment/' )
+            .then( response => response.json() )
+            .then( (responseJson) => {
+                console.log(responseJson);
+                this.assignments = responseJson;
+            })
+            .catch( (err) => {
+                console.error(err);
+            })
+        }
+       
+    },
+    created() {
+        this.fetchRefereeData();
+    }
+  
+  }
+  
+  Vue.createApp(SomeApp).mount('#soccerApp');
